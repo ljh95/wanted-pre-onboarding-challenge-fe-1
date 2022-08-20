9e6ffe2f29cead55cdd2cc3f265791d6057a5134
ljh95 <ilegwmam@gmail.com>
2022-08-20 16:09:09 +0900

    feat(Navigation): devide navigation component

---
+++ front/src/component/Navigation.tsx
@@ -0,0 +1,17 @@
+import { URL } from '@/func/url';
+import { Link } from 'react-router-dom';
+
+export const Navigation = () => {
+  return (
+    <nav>
+      <ul>
+        <li>
+          <Link to={URL.login}>로그인</Link>
+        </li>
+        <li>
+          <Link to={URL.signin}>회원가입</Link>
+        </li>
+      </ul>
+    </nav>
+  );
+};
